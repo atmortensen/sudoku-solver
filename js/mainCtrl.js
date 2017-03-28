554f9cb05d82a3e5736b028ff8b9c0a63fc24315
Alexand Mortensen <alextmortensen@gmail.com>
2017-03-27 22:22:48 -0600

    one more

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -2,16 +2,16 @@
 app.controller('mainCtrl', ['$scope', 'mainService', function($scope, mainService){
 
 	$scope.reset = function(){
-		$scope.puzzle = [["8",null,null,null,null,null,null,null,null],[null,null,"3","6","",null,null,null,null],[null,"7",null,null,"9",null,"2",null,null],[null,"5",null,null,null,"7",null,null,null],[null,null,null,null,"4","5","7",null,null],[null,null,null,"1",null,null,null,"3",null],[null,null,"1",null,null,null,null,"6","8"],[null,null,"8","5",null,null,null,"1",null],[null,"9",null,null,null,null,"4",null,null]];
-	     // $scope.puzzle = [[null,null,null,null,null,null,null,null,null],
-	     // 	             [null,null,null,null,null,null,null,null,null],
-	     // 	             [null,null,null,null,null,null,null,null,null],
-	     // 	             [null,null,null,null,null,null,null,null,null],
-	     // 	             [null,null,null,null,null,null,null,null,null],
-	     // 	             [null,null,null,null,null,null,null,null,null],
-	     // 	             [null,null,null,null,null,null,null,null,null],
-	     // 	             [null,null,null,null,null,null,null,null,null],
-	     // 	             [null,null,null,null,null,null,null,null,null]];
+		// $scope.puzzle = [["8",null,null,null,null,null,null,null,null],[null,null,"3","6","",null,null,null,null],[null,"7",null,null,"9",null,"2",null,null],[null,"5",null,null,null,"7",null,null,null],[null,null,null,null,"4","5","7",null,null],[null,null,null,"1",null,null,null,"3",null],[null,null,"1",null,null,null,null,"6","8"],[null,null,"8","5",null,null,null,"1",null],[null,"9",null,null,null,null,"4",null,null]];
+	     $scope.puzzle = [[null,null,null,null,null,null,null,null,null],
+	     	             [null,null,null,null,null,null,null,null,null],
+	     	             [null,null,null,null,null,null,null,null,null],
+	     	             [null,null,null,null,null,null,null,null,null],
+	     	             [null,null,null,null,null,null,null,null,null],
+	     	             [null,null,null,null,null,null,null,null,null],
+	     	             [null,null,null,null,null,null,null,null,null],
+	     	             [null,null,null,null,null,null,null,null,null],
+	     	             [null,null,null,null,null,null,null,null,null]];
 
 		$scope.error = '';
 		$scope.message = '';
