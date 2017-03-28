879115279be7e79e7434ce2ed3955895bd417093
Alexand Mortensen <alextmortensen@gmail.com>
2017-03-28 16:06:21 -0600

    added backspace functionality and moved jquery to a service

---
+++ js/autoCursor.js
@@ -0,0 +1,21 @@
+app.service('autoCursor', ['$document', function($document){
+
+	$document.ready(function(){
+		$('.input').keyup(function(e){
+			if(e.which===8){
+	        	var pre = $(this).parent().prev().find('.input');
+	        	if(pre.length===0){
+	        		pre = $(this).parent().parent().prev().find('.input').last();
+	        	}
+		        pre.focus();
+			} else {
+	        	var next = $(this).parent().next().find('.input');
+	        	if(next.length===0){
+	        		next = $(this).parent().parent().next().find('.input').first();
+	        	}
+		        next.focus();
+			}
+		}); 	
+	},1000);
+
+}]);
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,5 +1,5 @@
 // CONTROLLER
-app.controller('mainCtrl', ['$scope', 'mainService', function($scope, mainService){
+app.controller('mainCtrl', ['$scope', 'mainService', 'autoCursor', function($scope, mainService, autoCursor){
 
 	$scope.reset = function(){
 		// $scope.puzzle = [["8",null,null,null,null,null,null,null,null],[null,null,"3","6","",null,null,null,null],[null,"7",null,null,"9",null,"2",null,null],[null,"5",null,null,null,"7",null,null,null],[null,null,null,null,"4","5","7",null,null],[null,null,null,"1",null,null,null,"3",null],[null,null,"1",null,null,null,null,"6","8"],[null,null,"8","5",null,null,null,"1",null],[null,"9",null,null,null,null,"4",null,null]];
