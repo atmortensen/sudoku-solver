c379468c24c8d7bfd0f68e3a0afdecfc4467c3dd
Alexand Mortensen <alextmortensen@gmail.com>
2017-03-28 16:19:54 -0600

    Tab key fix

--- js/autoCursor.js
+++ js/autoCursor.js
@@ -2,20 +2,22 @@ app.service('autoCursor', ['$document', function($document){
 
 	$document.ready(function(){
 		$('.input').keyup(function(e){
-			alert(e.which);
-			if(e.which===8){
-	        	var pre = $(this).parent().prev().find('.input');
-	        	if(pre.length===0){
-	        		pre = $(this).parent().parent().prev().find('.input').last();
-	        	}
-		        pre.focus();
-			} else {
-	        	var next = $(this).parent().next().find('.input');
-	        	if(next.length===0){
-	        		next = $(this).parent().parent().next().find('.input').first();
-	        	}
-		        next.focus();
+			if(e.which!==9){
+				if(e.which===8){
+		        	var pre = $(this).parent().prev().find('.input');
+		        	if(pre.length===0){
+		        		pre = $(this).parent().parent().prev().find('.input').last();
+		        	}
+			        pre.focus();
+				} else {
+		        	var next = $(this).parent().next().find('.input');
+		        	if(next.length===0){
+		        		next = $(this).parent().parent().next().find('.input').first();
+		        	}
+			        next.focus();
+				}
 			}
+			
 		}); 	
 	},1000);
 
